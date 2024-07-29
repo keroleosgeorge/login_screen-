15af7c261af61beb7edd82bb56f0470d7b8f07ea
keroleosgeorge <georgekero79@gmail.com>
2024-07-29 07:49:59 +0300

    Messanger

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-import 'login_screen.dart';
+import 'Login_ui/login_screen.dart';
+import 'Messanger_ui/Messanger.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner:  false,
 
-      home: LoginScreen(),
+      home: Messanger(),
     );
   }
 }
